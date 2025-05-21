dd0289a07ee3b5af824d6230950e3eb25babeb35
KseniaShalagina <Kseniashalagina2803@gmail.com>
2025-05-21 19:10:59 +0000

    react начало

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import '../../assets/styles/Header.css'
+function Header(){
+    return (
+        <header>
+      <div className="headers">
+        <div>
+          <a href="#" className="logo"><b>Jobly</b></a>
+        </div>
+        <div>
+          <nav className="butt">
+            <ul>
+              <li>
+                <button className="logs"><b>For Recruiters</b></button>
+                <button className="sing"><b>Sign Up</b></button>
+                <button className="log"><b>Log in</b></button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+    );
+}
+export default Header;
