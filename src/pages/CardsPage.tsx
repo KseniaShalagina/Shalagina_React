a7d6ee9c0a2cd169495313ffef86be4a06b6afec
KseniaShalagina <Kseniashalagina2803@gmail.com>
2025-06-23 19:42:52 +0000

    cards

--- src/pages/CardsPage.tsx
+++ src/pages/CardsPage.tsx
@@ -1,14 +1,12 @@
 import { useSearchParams } from "react-router-dom";
 import CardList from '../components/CardList/CardList'
 import '../assets/styles/CardName.css'
-import { useLocation } from "react-router";
 
 const CARDS_LIMIT = 10;
 
 function CardsPage() {
-    const { search } = useLocation();
-    const searchParams = new URLSearchParams(search);
-    const limit = searchParams.get('limit') || CARDS_LIMIT;
+    const [searchParams] = useSearchParams();
+    const limit = Number(searchParams.get('limit')) || CARDS_LIMIT;
     return (
         <>
             <div className='cardName'>КАРТОЧКИ</div>
